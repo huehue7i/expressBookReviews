d0e8faf970b31f99205c290f6d6de832a2db8b53
huehue <145002938+huehue7i@users.noreply.github.com>
2025-08-23 16:08:41 +0530

    fix some issue

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,29 +20,56 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/books', async (req, res) => {
-    try {
-        const books = require('./booksdb.js');
-        res.status(200).json(JSON.stringify(books, null, 2));
-    } catch (error) {
-        res.status(500).json({ message: 'Unable to fetch books' });
+public_users.get('/books', (req, res) => {
+    function fetchBooks(callback) {
+        try {
+            const books = require('./booksdb.js');
+            callback(null, books);
+        } catch (error) {
+            callback(error);
+        }
     }
+
+    fetchBooks((err, data) => {
+        if (err) {
+            res.status(500).json({ message: 'Unable to fetch books', error: err.message });
+            return;
+        }
+        res.status(200).json(data);
+    });
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn', async (req, res) => {
-    try {
-        const books = require('./booksdb.js');
-        const isbn = req.params.isbn;
-        if (books[isbn]) {
-            // Simulating async operation
-            res.status(200).json(books[isbn]);
-        } else {
-            res.status(404).json({ message: "Book not found" });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Error fetching book by ISBN' });
+public_users.get('/isbn/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+
+    function findBookByISBN(isbn) {
+        return new Promise((resolve, reject) => {
+            try {
+                const books = require('./booksdb.js');
+                const book = books[isbn];
+                if (book) {
+                    resolve(book);
+                } else {
+                    reject(new Error('Book not found'));
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
     }
+
+    findBookByISBN(isbn)
+        .then(book => {
+            res.status(200).json(book);
+        })
+        .catch(error => {
+            if (error.message === 'Book not found') {
+                res.status(404).json({ message: 'Book not found with this ISBN' });
+            } else {
+                res.status(500).json({ message: 'Error fetching book by ISBN', error: error.message });
+            }
+        });
 });
   
 // Get book details based on author
@@ -87,21 +114,21 @@ public_users.get('/title/:title', async (req, res) => {
     }
 });
 
-
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  let result2 = [];
-  Object.keys(books).forEach(isbn => {
-      if (books[isbn].reviews) {
-          result2.push(books[isbn].reviews);
-      }
-      if (result2.length > 0){
-          res.status(200).json(result2);
-      } else {
-          res.status(404).json({message: "Review not found"});
-      }
-  })
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    const book = books[isbn];
+
+    if (!book) {
+        return res.status(404).json({ message: "Book not found" });
+    }
+
+    const reviews = book.reviews || {};
+    if (Object.keys(reviews).length === 0) {
+        return res.status(404).json({ message: "Review not found" });
+    }
+
+    return res.status(200).json(reviews);
 });
 
 module.exports.general = public_users;
